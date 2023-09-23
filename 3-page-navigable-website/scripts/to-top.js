090dd078e03deae3d748b019f815690510dffed2
Seth Bauer <seth@devpipeline.com>
2023-09-22 20:09:51 -0600

    Even prettier

---
+++ 3-page-navigable-website/scripts/to-top.js
@@ -0,0 +1,65 @@
+const topButton = document.getElementById("top-button");
+let onScreen = false;
+
+function buttonEnter() {
+  onScreen = true;
+  let id = null;
+  let pos = -100;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos === 50) {
+      clearInterval(id);
+    } else {
+      pos += 5;
+      topButton.style.bottom = pos + "px";
+    }
+  }
+}
+
+function buttonExit() {
+  onScreen = false;
+  let id = null;
+  let pos = 50;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos === -100) {
+      clearInterval(id);
+    } else {
+      pos -= 5;
+      topButton.style.bottom = pos + "px";
+    }
+  }
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    (onScreen && document.documentElement.scrollTop > 880) ||
+    (!onScreen && !(document.documentElement.scrollTop > 880))
+  ) {
+    console.log("no change");
+    return;
+  } else if (
+    onScreen &&
+    !(
+      document.documentElement.scrollTop > 880 ||
+      document.documentElement.scrollTop ===
+        document.documentElement.scrollHeight
+    )
+  ) {
+    buttonExit();
+    console.log("exit");
+  } else {
+    buttonEnter();
+    console.log("enter");
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
