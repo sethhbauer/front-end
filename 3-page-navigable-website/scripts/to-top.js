df4c7044c52014b008bed465e05d04a239a9b380
sethhbauer <144181428+sethhbauer@users.noreply.github.com>
2023-09-25 18:56:40 -0600

    capstone (#5)

--- 3-page-navigable-website/scripts/to-top.js
+++ 3-page-navigable-website/scripts/to-top.js
@@ -2,68 +2,6 @@ const topButton = document.getElementById("top-button");
 let onScreen = false;
 
 function buttonEnter() {
-<<<<<<< HEAD
-  onScreen = true;
-  let id = null;
-  let pos = -100;
-  clearInterval(id);
-  id = setInterval(frame, 5);
-  function frame() {
-    if (pos === 50) {
-      clearInterval(id);
-    } else {
-      pos += 5;
-      topButton.style.bottom = pos + "px";
-    }
-  }
-}
-
-function buttonExit() {
-  onScreen = false;
-  let id = null;
-  let pos = 50;
-  clearInterval(id);
-  id = setInterval(frame, 5);
-  function frame() {
-    if (pos === -100) {
-      clearInterval(id);
-    } else {
-      pos -= 5;
-      topButton.style.bottom = pos + "px";
-    }
-  }
-}
-
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    (onScreen && document.documentElement.scrollTop > 880) ||
-    (!onScreen && !(document.documentElement.scrollTop > 880))
-  ) {
-    console.log("no change");
-    return;
-  } else if (
-    onScreen &&
-    !(
-      document.documentElement.scrollTop > 880 ||
-      document.documentElement.scrollTop ===
-        document.documentElement.scrollHeight
-    )
-  ) {
-    buttonExit();
-    console.log("exit");
-  } else {
-    buttonEnter();
-    console.log("enter");
-  }
-}
-
-function topFunction() {
-  document.documentElement.scrollTop = 0;
-=======
 	onScreen = true;
 	let id = null;
 	let pos = -100;
@@ -120,5 +58,4 @@ function scrollFunction() {
 
 function topFunction() {
 	document.documentElement.scrollTop = 0;
->>>>>>> dev
 }
